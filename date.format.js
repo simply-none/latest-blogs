9897bfbf9522663083479fcabb3d6406b45324b9
200803001 <200803001@sitir.net>
2023-06-02 14:11:45 +0800

    fix🐛:

--- date.format.js
+++ date.format.js
@@ -3,7 +3,7 @@ module.exports = {
 		var date = new Date(time);
 
 		var year = date.getFullYear(),
-			month = ('' + date.getMonth() + 1).padStart(2, '0'), //月份是从0开始的
+			month = (date.getMonth() + 1 + '').padStart(2, '0'), //月份是从0开始的
 			day = ('' + date.getDate()).padStart(2, '0'),
 			hour = ('' + date.getHours()).padStart(2, '0'),
 			min = ('' + date.getMinutes()).padStart(2, '0'),
