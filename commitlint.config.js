5c12ed550486af85637f916ab7adb369bf4e9f58
200803001 <200803001@sitir.net>
2023-06-07 10:47:24 +0800

    fix(d): d
    d

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = { 
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-atom",
+  formatter: "@commitlint/format",
+  rules: {
+    "type-enum": [2, "always", ["foo"]]
+  },
+  ignores: [
+    commit => commit === 'jousindea'
+  ],
+  defaultIgnores: true,
+  prompt: {
+    message: {},
+    questions: {
+      type: {
+        description: "please input type:"
+      }
+    }
+  }
+};
