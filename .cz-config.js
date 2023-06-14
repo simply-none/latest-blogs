310ae4e952820c679b29a5745a89f138b88c0d7c
200803001 <200803001@sitir.net>
2023-06-14 14:25:14 +0800

    docs(配置文件): 配置文件修改

--- .cz-config.js
+++ .cz-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   //可选类型
   types: [
+    { value: 'docs', name: 'docs:   文档变更' },
     { value: 'feat', name: 'feat:   新功能' },
     { value: 'fix', name: 'fix:   修复' },
-    { value: 'docs', name: 'docs:   文档变更' },
     { value: 'style', name: 'style:   代码格式(不影响代码运行的变动)' },
     {
       value: 'refactor',
@@ -27,7 +27,13 @@ module.exports = {
     footer: '请输入要关闭的issue(可选)',
     confirmCommit: '确认以上信息提交?(y/n)'
   },
-  scopes: [{ name: '笔记' }, { name: '配置文件' }, { name: '其他' }],
+  scopes: [
+    { name: '修改笔记' },
+    { name: '新增笔记' },
+    { name: '删除笔记' },
+    { name: '配置文件' },
+    { name: '其他' }
+  ],
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   //跳过问题
