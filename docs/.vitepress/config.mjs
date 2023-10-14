99deddf88120c7be8582afcdd01de0b061054ed2
jousindea <jousindea@163.com>
2023-10-14 14:29:59 +0800

    fix(custom): 修复错误

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -39,12 +39,60 @@ export default defineConfig({
     footer: {
       copyright: "内容整理权归解璎所有 Copyright © 2022-present",
     },
+    algolia: {
+      appId: 'N4M009QH1O',
+      apiKey: '62038b1e84670a59365b0dfc75595345',
+      indexName: 'blogs'
+    },
     search: {
       provider: 'algolia',
       options: {
         appId: 'N4M009QH1O',
         apiKey: '62038b1e84670a59365b0dfc75595345',
-        indexName: 'blogs'
+        indexName: 'blogs',
+        locales: {
+          zh: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
+                }
+              }
+            }
+          }
+        }
       }
     }
   },
